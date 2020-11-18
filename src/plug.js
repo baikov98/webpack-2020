07ce4eab6a0477dc4478b5f03bddb509b015a4a5
Vilmir Baikov <vilmir.baikoff@yandex.ru>
2020-11-18 19:10:57 +0500

    Тренировочный калькулятор jquery

--- src/plug.js
+++ src/plug.js
@@ -29,7 +29,7 @@ $(document).ready(() => {
     $('.iqdropdown').iqDropdown();
   }); 
 
-$(document).ready(() => {
+/* $(document).ready(() => {
     function checkval() {
       return $(this).siblings('.item__value').text()
     }
@@ -51,4 +51,74 @@ $(document).ready(() => {
       $(this).siblings('.item__value').text(toString(val)); 
   });
     
+  });  */
+
+  $(document).ready(() => {
+    function calculate () {
+      const total = Number($('#total-length').val()) * 100;
+      let usedPerDay = 0;
+      let result = 0;
+      $('.control__input', '#users-list').each(function (index, item) {
+        usedPerDay = usedPerDay + Number($(item).val())
+      });
+      if ($('.control__button--active').data('type') === 'paper') {
+        const papersCount = total / Number($('#paper-length').val())
+        result = papersCount / usedPerDay;
+      } else{
+        result = total / usedPerDay
+      }
+
+      $('#result').html(result.toFixed(0))
+
+    };
+    function generateUsers () {
+     $('#users-list').empty();
+      console.log('vizov')
+      const currentType = $('.control__button--active').data('type') === 'paper' ? 
+        {unit: 'листочков', defaultValue: '1'} : {unit: 'см', defaultValue: '10'};
+        console.log(currentType)
+      for (let i = 1; i <= $('#users-count').val(); i++){
+        $('#users-list').append(`
+          <div class="form-group form-group--row">
+            <div class="form-group__title">Человек ${i} (${currentType.unit})</div>
+            <div class="form-group__control control">
+              <input class="control__input" type="text" value=${currentType.defaultValue} />
+            </div>
+          </div>
+        `)
+      }
+      calculate()
+    };
+    $('#total-length, #paper-length').blur(calculate);
+    $('#users-count').blur(generateUsers);
+    
+    $('body').on('change', '#users-list .control__input', function() {
+      calculate()
+    })
+
+    $('.control__button').click(function(){
+      $(this)
+          .addClass('control__button--active')
+          .siblings()
+          .removeClass('control__button--active')
+      if ($(this).data('type') === 'paper') {
+        $('#paper-length-block').removeClass('hidden')
+      } else {
+        $('#paper-length-block').addClass('hidden')
+      }
+      generateUsers();
+    });
+
+    generateUsers()
+    calculate ()
   }); 
+
+
+
+
+
+
+
+
+
+
